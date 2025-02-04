9d752b24e4edcf37f0520eb117847290ef67c7f8
ElieOko <elieoko100@gmail.com>
2025-02-05 00:59:40 +0100

    fix:controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,4 +5,10 @@ namespace App\Http\Controllers;
 abstract class Controller
 {
     //
+    public $msg_success = "Enregistrement réussie avec succès";
+    public $msg_error = "Ressource non trouvé";
+    public $msg_account_invalid = "Compte Invalide";
+    public $msg_email_found_error = "Cette adresse mail appartient a un membre de la communauté";
+    public $status_ok = 200;
+    public $status_error = 404;
 }
--- app/Http/Controllers/TypeBedRoomController.php
+++ app/Http/Controllers/TypeBedRoomController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\TypeBedRoom;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class TypeBedRoomController extends Controller
 {
@@ -28,7 +29,25 @@ class TypeBedRoomController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(),[
+            'nom' =>'required',
+            'parent_space_id'=>'required',
+            'branch_id'=>'required'
+        ]);
+        if($validator->stopOnFirstFailure()->fails()){
+            return response()->json([
+                'message' => $validator->errors()
+             ],402);
+        }
+        $field = $validator->validated();
+        TypeBedRoom::updateOrCreate([
+            'nom'               =>   $field['nom'],
+            'parent_space_id'   =>   $field['parent_space_id'],
+            'branch_id'         =>   $field['branch_id']
+        ]);
+        return response()->json([
+            'message' => $this->msg_success,
+         ],$this->status_ok);
     }
 
     /**
