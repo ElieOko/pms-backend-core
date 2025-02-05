3fab88eaa4d2d326cd9af6f2146d397b5baf25d0
Espoir Kakesa <120624185+Kakesa@users.noreply.github.com>
2025-02-05 06:35:33 +0100

    ajout du backend dans la parti store de controller type et leurs routes dans le fichier api

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,33 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TypePlatController;
+use App\Http\Controllers\TypeClientController;
 use App\Http\Controllers\TypeBedRoomController;
+use App\Http\Controllers\TypeCantineController;
+use App\Http\Controllers\TypeCuisineController;
+use App\Http\Controllers\TypeProgrammeFidelisationController;
 
 
 Route::resource('type/bedroom', TypeBedRoomController::class)
 ->only(['index', 'show','edit','store']);
+
+Route::resource('type/client', TypeClientController::class)
+->only(['index', 'show','edit','store']);
+
+Route::resource('type/cuisine', TypeCuisineController::class)
+->only(['index', 'show','edit','store']);
+
+Route::resource('type/cantine', TypeCantineController::class)
+->only(['index', 'show','edit','store']);
+
+Route::resource('type/plat', TypePlatController::class)
+->only(['index', 'show','edit','store']);
+
+Route::resource('type/programme/fidelisation', TypeProgrammeFidelisationController::class)
+->only(['index', 'show','edit','store']);
+
+
 
 Route::middleware(['auth:sanctum'])->group(function () {
     //protected
